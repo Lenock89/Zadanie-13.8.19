eedc4736c3014163a974c7002b381573a384b83c
Lenock89 <123875874+Lenock89@users.noreply.github.com>
2023-02-16 20:01:43 +0300

    Update zadanie 13.8.19.py

--- zadanie 13.8.19.py
+++ zadanie 13.8.19.py
@@ -1,1 +1,20 @@
+tickets = int(input('Введите количество билетов: '))
+numbers_ages = []
+for i in range(0, tickets):
+    input_value = int(input(f'Введите возраст участника №{i + 1}:\n'))
+    numbers_ages.append(input_value)
 
+    def price(age):
+        if age < 18:
+            return 0
+        elif 18 <= age < 25:
+            return 990
+        else:
+            return 1390
+
+    final_price = sum(map(price, numbers_ages))
+discount_price = int(final_price * 0.9)
+if tickets > 3:
+    print('Стоимость всех билетов со скидкой: ', discount_price, "руб.")
+else:
+    print('Стоимость всех билетов: ', final_price, "руб.")
